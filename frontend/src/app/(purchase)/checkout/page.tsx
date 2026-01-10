0182a0da91ca86ed01e1caf7e04d409b4aadd11d
ShotaGhoona <shota.yamashita@ghoona.com>
2026-01-10 21:39:30 +0900

    feat: frontend payment sec -design

---
+++ frontend/src/app/(purchase)/checkout/page.tsx
@@ -0,0 +1,11 @@
+import type { Metadata } from 'next';
+import { CheckoutContainer } from '@/page-components/purchase/checkout/ui/CheckoutContainer';
+
+export const metadata: Metadata = {
+  title: '購入手続き | ACRIQUE',
+  description: '配送先とお支払い方法を選択してください。',
+};
+
+export default function Page() {
+  return <CheckoutContainer />;
+}

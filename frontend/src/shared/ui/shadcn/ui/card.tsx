268930c09dca45ba2e80cfbb2a117fcb2a9eaa37
ShotaGhoona <shota.yamashita@ghoona.com>
2026-01-11 00:45:20 +0900

    fix: card design

--- frontend/src/shared/ui/shadcn/ui/card.tsx
+++ frontend/src/shared/ui/shadcn/ui/card.tsx
@@ -7,7 +7,7 @@ function Card({ className, ...props }: React.ComponentProps<'div'>) {
     <div
       data-slot='card'
       className={cn(
-        'flex flex-col gap-6 rounded-xl border bg-card py-6 text-card-foreground shadow-sm',
+        'flex flex-col gap-6 rounded-sm border bg-background py-6 text-foreground',
         className,
       )}
       {...props}
@@ -32,7 +32,7 @@ function CardTitle({ className, ...props }: React.ComponentProps<'div'>) {
   return (
     <div
       data-slot='card-title'
-      className={cn('font-semibold leading-none', className)}
+      className={cn('font-medium tracking-wide leading-none', className)}
       {...props}
     />
   );

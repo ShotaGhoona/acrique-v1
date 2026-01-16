d0d33576de21ac4d4e8bf9e48fc50fbb6668cd18
ShotaGhoona <shota.yamashita@ghoona.com>
2026-01-16 19:57:40 +0900

    fix: fixed headerによる見切れ対策でmypage/purchaseにpt-16追加

--- frontend/src/app/(purchase)/layout.tsx
+++ frontend/src/app/(purchase)/layout.tsx
@@ -17,7 +17,7 @@ export default function PurchaseLayout({
   return (
     <div className='flex min-h-screen flex-col'>
       <Header />
-      <main className='flex-1 bg-secondary/20'>{children}</main>
+      <main className='flex-1 bg-secondary/20 pt-16'>{children}</main>
       <Footer />
     </div>
   );
